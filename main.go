57ddff633143a31a9642f1cd103a7d4794a750e6
Sebastian Łuszczek <sebastianluszczek@pop-os.localdomain>
2021-11-02 12:44:33 +0100

    api returning time

--- main.go
+++ main.go
@@ -12,10 +12,11 @@ import (
 
 type timeEvent struct {
 	Time string `json:"time"`
+	Message string `json:"message"`
 }
 
 func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	t := timeEvent{Time: time.Now().String()}
+	t := timeEvent{Time: time.Now().String(), Message: "Hello World"}
 	b, err := json.Marshal(t)
 	if err != nil {
 		return events.APIGatewayProxyResponse{}, err
